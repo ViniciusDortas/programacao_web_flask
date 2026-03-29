a08e37e35cfccdad8c81757b96dc4503eae1a741
Vinicius Dortas <viniciusdortasgama@gmail.com>
2026-03-29 19:31:32 -0300

    criando os forms

---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class FormCriarConta(FlaskForm):
+    username = StringField("Nome de Usuário", validators=[DataRequired(), Length(1,30)])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6,20)])
+    confirmacao = PasswordField("Confirmação da Senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_submit = SubmitField("Criar Conta")
+
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6,20)])
+    botao_submit = SubmitField("Fazer Login")
+
+
+    
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
 
 lista_usuarios = ["João", "Pedro", "Fernanda", "Alessandro", "Maria"]
+
+app.config["SECRET_KEY"] = "215addf1fa33ea685623b7e000281b3f"
+
 
 @app.route("/")
 def home():
@@ -17,6 +21,13 @@ def contato():
 @app.route("/usuarios")
 def usuarios():
     return render_template("usuarios.html", lista_usuarios=lista_usuarios)
+
+
+@app.route("/login")
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template("login.html", form_login=form_login, form_criarconta=form_criarconta)
 
 
 if __name__ == "__main__":
